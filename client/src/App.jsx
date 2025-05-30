8467bbca3a59434b2359dde4b5872fd95850ca1b
Minhyuk12 <sunyart12@gmail.com>
2025-05-30 22:59:28 +0900

    make profile page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-
+import Profile from "./pages/Profile";
 export default function App() {
   // 예시: 로그인 상태를 useState로 관리
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -19,6 +19,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
