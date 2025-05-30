857cb5557adc98be09287cc286fc32b44a8b2171
Kim Minseo <minqseo0630@naver.com>
2025-05-30 19:34:40 +0900

    [#7] UI: Split Navbar into Auth and Guest components and wire up in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
+// src/App.jsx
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 
 export default function App() {
+  // 예시: 로그인 상태를 useState로 관리
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <BrowserRouter>
+      {/* 네비게이션은 항상 보이도록 App 레벨에서 렌더링 */}
+      <Navbar isLoggedIn={isLoggedIn} />
+
       <Routes>
         <Route path="/" element={<Home />} />
+        {/* 나중에 Profile, MyScore, Login, Register 페이지도 여기에 추가 */}
       </Routes>
     </BrowserRouter>
   );
