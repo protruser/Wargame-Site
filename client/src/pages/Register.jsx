4517ce9c111379eecb4405bfaadd192c2b5f8823
Minhyuk12 <sunyart12@gmail.com>
2025-05-30 19:42:51 +0900

    make Register and merge navbar

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+function Register() {
+  return (
+    <div className="fixed inset-0 bg-gray-100 flex justify-center items-center">
+      <div className="w-full max-w-xl bg-white p-10 shadow-md rounded">
+        <h1 className="text-3xl font-semibold mb-8 mt-8 text-center text-black border-b pb-4">Register</h1>
+
+        <form className="space-y-6">
+          <div>
+            <label className="block mb-1 font-medium text-gray-700">Username</label>
+            <input
+              type="text"
+              placeholder="Enter username"
+              className="w-full bg-gray-200 px-4 py-2 rounded text-black"
+            />
+            <p className="text-sm text-gray-500 mt-1">your username on the site</p>
+          </div>
+
+          <div>
+            <label className="block mb-1 font-medium text-gray-700">Email</label>
+            <input
+              type="email"
+              placeholder="Enter email"
+              className="w-full bg-gray-200 px-4 py-2 rounded text-black"
+            />
+            <p className="text-sm text-gray-500 mt-1">
+              Email must include "@" and a domain
+            </p>
+          </div>
+
+          <div>
+            <label className="block mb-1 font-medium text-gray-700">Password</label>
+            <input
+              type="password"
+              placeholder="Enter password"
+              className="w-full bg-gray-200 px-4 py-2 rounded text-black"
+            />
+            <p className="text-sm text-gray-500 mt-1">
+                At least 8 characters, incl. uppercase, lowercase, and special character
+            </p>
+          </div>
+
+          <div className="flex justify-end">
+            <button
+              type="submit"
+              className="bg-gray-300 text-black px-6 py-2 rounded hover:bg-gray-400"
+            >
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
