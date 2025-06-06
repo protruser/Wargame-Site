134c4acbc34ea562134d74aa5f06cc3c1d2c766e
protruser <marokk9768@gmail.com>
2025-06-06 18:15:21 +0900

    (feature): delete method

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
+const verifyToken = require('../middlewares/authMiddleware');
 
 router.post('/add', userController.addUser);
 router.get('/statistics', userController.getStatistics);
+router.delete('/:user_id', verifyToken, userController.deleteAccount);
 
 module.exports = router;
