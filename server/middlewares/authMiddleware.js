417a750bd470c4e509617740595baabb3ea4d2d2
Inyong Hwang <hwanginyong@seoultech.ac.kr>
2025-06-06 17:05:26 +0900

    Challenge

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = process.env.JWT_SECRET || 'fallback_secret';
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ error: 'No token provided' });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, SECRET_KEY);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return res.status(401).json({ error: 'Invalid token' });
+    }
+};
+
+module.exports = verifyToken;
