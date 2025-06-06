d953feb3539a4405d443b2d17eee53a279f11a0e
Kim Minseo <minqseo0630@naver.com>
2025-06-06 10:51:24 +0900

    임시 커밋: main 병합 전 로컬 변경사항 저장

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,13 +1,13 @@
 // src/pages/home/Home.jsx
 import React from "react";
 import HeroSection from "./HeroSection";
-import FeatureTimeline from "./FeatureTimeline";
+import FeatureList from "./FeatureList";
 
 export default function Home() {
   return (
     <main className="pt-[60px]">
       <HeroSection />
-      <FeatureTimeline />
+      <FeatureList />
     </main>
   );
 }
